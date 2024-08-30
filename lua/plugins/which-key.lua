dcb424491c072b04c46bdf215409412db3066056
Jihan Jasper Al Rashid <j.jasper.a@gmail.com>
2024-08-30 13:58:37 +0600

    feat: add some keymaps for neo-tree

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,20 +1,37 @@
 return {
-	{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+    },
+    keys = {
+      -- Buffer Local Keymaps
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+      -- Neotree: Show File Explorer
+      {
+        "<leader>e",  -- Keybinding to show Neotree
+        ":Neotree show<CR>",
+        desc = "Show Neotree",
+      },
+      -- Neotree: Focus Neotree with Ctrl + Left
+      {
+        "<C-Left>",  -- Ctrl + Left arrow to focus Neotree
+        ":Neotree focus<CR>",
+        desc = "Focus Neotree",
+      },
+      -- Focus file with Ctrl + Right
+      {
+        "<C-Right>",  -- Ctrl + Right arrow to focus the file
+        "<C-w>l",  -- Ctrl-w followed by l to move focus to the right (where the file is)
+        desc = "Focus File",
+      },
     },
   },
-}
 }
